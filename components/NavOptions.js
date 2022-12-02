7db1dccc15fc70bf4efdd4f53e6f3092ff02b3ff
nickssilver <51230602+nickssilver@users.noreply.github.com>
2022-12-03 02:37:50 +0300

    navigation set

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList } from 'react-native-web';
+import tw from "tailwind-react-native-classnames";
+import { Icon } from '@rneui/themed';
+
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image:"https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order food",
+        image:"https://links.papareact.com/28w",
+        screen: "EatsScreen", // change later
+    },
+    
+];
+
+
+
 
 const NavOptions= () => {
     return(
+      <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({item}) =>  (
+        <TouchableOpacity 
+        style= {tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
         <view>
-          <text>Hey</text>  
+
+            <Image
+style={{width: 120, height: 120, resizeMode: "contain"}}
+
+            source = {{uri: item.image}}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <icon style={tw`mt-2 bg-black rounded-full w-10 mt-4`}
+            name="arrowright" color="white"
+            type="antdesign"
+            />
+
         </view>
+        </TouchableOpacity>
+
+      )}
+
+      />
     );
 };
 
