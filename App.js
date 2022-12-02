9d5e928c1bf2de5c4f32e62e4659ef44bbe5727f
nickssilver <51230602+nickssilver@users.noreply.github.com>
2022-12-03 00:12:08 +0300

    init

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+
+
+
 export default function App() {
   return (
-    <Provider>
-    <View style={styles.container}>
-      <Text>Let's build Uber</Text>
-   
-    </View>
+    <Provider store={store}>
+    <HomeScreen/>
+  
     </Provider>
   );
 }
